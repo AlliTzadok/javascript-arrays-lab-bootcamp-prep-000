194816a9e01bb30711879d036ca02201cfab282b
Allison Tzadok <allisontzadok@gmail.com>
2017-12-04 06:42:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  var newKitten = [...kittens,"broom"]
+  var newKitten = [...kittens,"Broom"]
   return newKitten;
 }
